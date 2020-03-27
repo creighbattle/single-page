8db02ce4e956d51af9fd9d830bc9b2068566dc5a
Creigh Battle <creighbattle@Creighs-MacBook-Air.local>
2020-03-26 22:18:01 -0400

    Working Movie Database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
+
+import Search from './components/Search'
+import Results from './components/Results'
 
 function App() {
+
+  const [state, setState] = useState({
+    s: '',
+    results: [],
+    selected: {}
+  });
+
+  const apiurl ="http://www.omdbapi.com/?i=tt3896198&apikey=5b280ac0"
+
+  const search = (e) => {
+    if(e.key === 'Enter') {
+      axios(apiurl + "&s=" + state.s).then(({data}) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results}
+        })
+      });
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+
+  }
+
   return (
     <div>
-      <h1>Creigh Battle</h1>
+      <header>
+        <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} />
+      </main>
     </div>
   );
 }
